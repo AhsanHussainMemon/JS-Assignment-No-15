a3f572f309a9b6eaa418bbe1433510e4042aa004
AhsanHussainMemon <hussainahsan454@gmail.com>
2024-10-23 15:08:58 +0500

    Add files via upload

---
+++ cal.js
@@ -0,0 +1,18 @@
+// var outputscreen = document.getElementById("outputscreen");
+
+// function display(value) {
+//     outputscreen.value += value;
+// }
+
+// function clearScreen() {
+//     outputscreen.value = "";
+// }
+
+
+// function del() {
+//     outputscreen.value = outputscreen.value.slice(0, -1);
+// }
+
+// function Calculate() {
+//     outputscreen.value = eval(outputscreen.value);
+// }
